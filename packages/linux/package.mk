f7fd1c1c6c9ac0ae10086b684217adefbc29051b
alex <surkov.alex@gmail.com>
2016-04-18 22:41:29 +0200

    [aml] kernel: udpate to 3.14-63371bc

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -32,7 +32,7 @@ PKG_LONGDESC="This package contains a precompiled kernel image and the modules."
 case "$LINUX" in
   amlogic)
     if [ "$TARGET_KERNEL_ARCH" = "arm64" ]; then
-      PKG_VERSION="amlogic-3.14-9202612"
+      PKG_VERSION="amlogic-3.14-63371bc"
       PKG_URL="http://amlinux.ru/source/$PKG_NAME-$PKG_VERSION.tar.gz"
     else
       PKG_VERSION="amlogic-3.10-c8d5b2f"
