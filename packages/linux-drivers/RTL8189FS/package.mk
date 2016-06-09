05ef06a47e702715d238884e36809f1c73a4453d
Oleg Ivanov <leo-sp50@yandex.ru>
2016-06-09 11:35:08 +0300

    merge 20160609
    	изменено:                   packages/audio/pulseaudio/package.mk
    	изменено:                   packages/graphics/opengl-meson64/package.mk
    	new file:                   packages/linux-drivers/RTL8189FS/package.mk
    	изменено:                   packages/linux-firmware/brcmfmac_sdio-firmware-aml/system.d/brcmfmac_sdio-firmware@.service
    	изменено:                   packages/linux-firmware/brcmfmac_sdio-firmware-aml/udev.d/80-brcmfmac_sdio.rules
    	изменено:                   packages/linux/package.mk
    	изменено:                   packages/mediacenter/kodi/scripts/kodi-config
    	изменено:                   projects/Amlogic/devices/MXV/linux/linux.aarch64.conf
    	new file:                   projects/Amlogic/devices/S905/filesystem/etc/amremote/remote.conf
    	new file:                   projects/Amlogic/devices/S905/filesystem/lib/firmware/brcm/nvram.txt
    	new file:                   projects/Amlogic/devices/S905/filesystem/usr/lib/modules-load.d/wireless.conf
    	new file:                   projects/Amlogic/devices/S905/filesystem/usr/share/kodi/system/keymaps/k1-remote.xml
    	new file:                   projects/Amlogic/devices/S905/install/files/logo.img
    	new file:                   projects/Amlogic/devices/S905/install/s905_autoscript.txt
    	new file:                   projects/Amlogic/devices/S905/install/update-binary
    	new file:                   projects/Amlogic/devices/S905/install/updater-script
    	new file:                   projects/Amlogic/devices/S905/linux/linux.aarch64.conf
    	new file:                   projects/Amlogic/devices/S905/options
    	new file:                   projects/Amlogic/devices/S905/patches/linux/0001-Revert-thermal-sync-with-SDK.patch
    	new file:                   projects/Amlogic/devices/S905/patches/linux/KI_Plus-dts.patch
    	изменено:                   projects/Amlogic/devices/SPOT_i7/linux/linux.aarch64.conf
    	изменено:                   projects/Amlogic/devices/Vega_S95/linux/linux.aarch64.conf
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/BCM4345C0.hcd
    	изменено:                   projects/Amlogic/filesystem/lib/firmware/brcm/config.txt
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/fw_bcm40181a0.bin
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/fw_bcm40181a0_apsta.bin
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/fw_bcm43455c0_ag.bin
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/fw_bcm43455c0_ag_apsta.bin
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/fw_bcm43455c0_ag_p2p.bin
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/nvram.txt
    	new file:                   projects/Amlogic/filesystem/lib/firmware/brcm/nvram_43458.txt
    	изменено:                   projects/Amlogic/options
    	new file:                   projects/Amlogic/patches/linux/auto-refresh-rate.black.screen.FIX.patch
    	изменено:                   scripts/image
    	изменено:                   scripts/mkimage

--- packages/linux-drivers/RTL8189FS/package.mk
+++ packages/linux-drivers/RTL8189FS/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,37 +16,37 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="opengl-meson64"
-PKG_VERSION="r5p1-01rel0"
+PKG_NAME="RTL8189FS"
+PKG_VERSION="e7108c7"
 PKG_REV="1"
-PKG_ARCH="aarch64"
-PKG_LICENSE="nonfree"
-PKG_SITE="http://openlinux.amlogic.com:8000/download/ARM/filesystem/"
-PKG_URL="http://amlinux.ru/source/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/150balbes/"
+PKG_GIT_URL="https://github.com/150balbes/Amlogic_s905-driver.git"
+PKG_GIT_BRANCH="RTL8189FS"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_PRIORITY="optional"
-PKG_SECTION="graphics"
-PKG_SHORTDESC="opengl-meson64: OpenGL ES pre-compiled libraries for Mali 450 GPUs found in Amlogic Meson64 SoCs"
-PKG_LONGDESC="opengl-meson64: OpenGL ES pre-compiled libraries for Mali 450 GPUs found in Amlogic Meson64 SoCs"
+PKG_SECTION="driver"
+PKG_SHORTDESC="RTL8189FS Linux 3.x driver"
+PKG_LONGDESC="RTL8189FS Linux 3.x driver"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+pre_make_target() {
+  unset LDFLAGS
+}
+
 make_target() {
- : # nothing todo
+  make V=1 \
+       ARCH=$TARGET_KERNEL_ARCH \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_PREFIX \
+       CONFIG_POWER_SAVING=n
 }
 
 makeinstall_target() {
-  mkdir -p $SYSROOT_PREFIX/usr/include
-    cp -PR usr/include/* $SYSROOT_PREFIX/usr/include
-
-  mkdir -p $SYSROOT_PREFIX/usr/lib
-    cp -PR usr/lib/*.so* $SYSROOT_PREFIX/usr/lib
-
-  mkdir -p $INSTALL/usr/lib
-    cp -PR usr/lib/*.so* $INSTALL/usr/lib
-}
-
-post_install() {
-  enable_service unbind-console.service
+  mkdir -p $INSTALL/lib/modules/$(get_module_dir)/$PKG_NAME
+    cp *.ko $INSTALL/lib/modules/$(get_module_dir)/$PKG_NAME
 }
